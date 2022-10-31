13f39b96f69a6e9a55307fa12db9ce6ba67ae6a1
TwelveT <2471835953@qq.com>
2022-10-31 15:33:50 +0800

    sa token

---
+++ twelvet-admin/src/test/java/com/twelvet/UtilsTest.java
@@ -0,0 +1,16 @@
+package com.twelvet;
+
+import cn.dev33.satoken.secure.BCrypt;
+
+/**
+ * @author twelvet
+ * @WebSite www.twelvet.cn
+ * @Description: 工具测试
+ */
+public class UtilsTest {
+
+    public static void main(String[] args) {
+        System.out.println(BCrypt.hashpw("123456"));
+    }
+
+}
