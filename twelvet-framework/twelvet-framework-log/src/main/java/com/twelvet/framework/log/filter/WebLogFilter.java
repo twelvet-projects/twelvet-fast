72d5de76f76553fb3c821048ed32e08dec02d8b8
TwelveT <2471835953@qq.com>
2022-12-05 17:18:09 +0800

    采用文本块

--- twelvet-framework/twelvet-framework-log/src/main/java/com/twelvet/framework/log/filter/WebLogFilter.java
+++ twelvet-framework/twelvet-framework-log/src/main/java/com/twelvet/framework/log/filter/WebLogFilter.java
@@ -15,6 +15,7 @@ import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
@@ -25,113 +26,113 @@ import java.util.Map;
 @Component
 public class WebLogFilter implements Filter {
 
-	private final static Logger log = LoggerFactory.getLogger(WebLogFilter.class);
+    private final static Logger log = LoggerFactory.getLogger(WebLogFilter.class);
 
-	/**
-	 * 忽略日志输出地址
-	 */
-	private final static List<String> IGNORES = Arrays.asList("/actuator/health");
+    /**
+     * 忽略日志输出地址
+     */
+    private final static List<String> IGNORES = Arrays.asList("/actuator/health");
 
-	@Override
-	public void init(FilterConfig filterConfig) {
-		log.info("过滤器初始化");
-	}
+    @Override
+    public void init(FilterConfig filterConfig) {
+        log.info("过滤器初始化");
+    }
 
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
 
-		HttpServletResponse servletResponse = (HttpServletResponse) response;
+        HttpServletResponse servletResponse = (HttpServletResponse) response;
 
-		HttpServletRequest servletRequest = (HttpServletRequest) request;
+        HttpServletRequest servletRequest = (HttpServletRequest) request;
 
-		// 忽略列表日志输出
-		if (IGNORES.contains(servletRequest.getRequestURI())) {
-			chain.doFilter(request, response);
-			return;
-		}
+        // 忽略列表日志输出
+        if (IGNORES.contains(servletRequest.getRequestURI())) {
+            chain.doFilter(request, response);
+            return;
+        }
 
-		String contentType = request.getContentType();
-		if (!TUtils.isEmpty(contentType)) {
-			if (contentType.startsWith(MediaType.MULTIPART_FORM_DATA_VALUE)
-					|| contentType.startsWith(MediaType.APPLICATION_FORM_URLENCODED_VALUE)) {
-				chain.doFilter(request, response);
-				return;
-			}
-		}
+        String contentType = request.getContentType();
+        if (!TUtils.isEmpty(contentType)) {
+            if (contentType.startsWith(MediaType.MULTIPART_FORM_DATA_VALUE)
+                    || contentType.startsWith(MediaType.APPLICATION_FORM_URLENCODED_VALUE)) {
+                chain.doFilter(request, response);
+                return;
+            }
+        }
 
-		long startTime = System.currentTimeMillis();
+        long startTime = System.currentTimeMillis();
 
-		RequestWrapper requestWrapper = new RequestWrapper((HttpServletRequest) request);
-		ResponseWrapper responseWrapper = new ResponseWrapper(servletResponse);
+        RequestWrapper requestWrapper = new RequestWrapper((HttpServletRequest) request);
+        ResponseWrapper responseWrapper = new ResponseWrapper(servletResponse);
 
-		String reqJson = requestWrapper.getBody();
-		Map<String, String[]> map = request.getParameterMap();
-		if (map != null && !map.isEmpty() && StringUtils.isEmpty(reqJson)) {
-			reqJson = mapToString(map);
-		}
-		requestWrapper.setAttribute("jsonParam", reqJson);
+        String reqJson = requestWrapper.getBody();
+        Map<String, String[]> map = request.getParameterMap();
+        if (map != null && !map.isEmpty() && StringUtils.isEmpty(reqJson)) {
+            reqJson = mapToString(map);
+        }
+        requestWrapper.setAttribute("jsonParam", reqJson);
 
-		chain.doFilter(requestWrapper, responseWrapper);
-		long endTime = System.currentTimeMillis();
-		// 获取response返回的内容并重新写入response
-		byte[] bytes = responseWrapper.getResponseData();
-		response.getOutputStream().write(bytes);
+        chain.doFilter(requestWrapper, responseWrapper);
+        long endTime = System.currentTimeMillis();
+        // 获取response返回的内容并重新写入response
+        byte[] bytes = responseWrapper.getResponseData();
+        response.getOutputStream().write(bytes);
 
-		// 仅输出JSON
-		String responseData = new String(bytes, StandardCharsets.UTF_8);
+        // 仅输出JSON
+        String responseData = new String(bytes, StandardCharsets.UTF_8);
 
-		// 判断是否为JSON响应
-		if (!JacksonUtils.isValidJson(responseData)) {
-			responseData = "IGNORES";
-		}
+        // 判断是否为JSON响应
+        if (!JacksonUtils.isValidJson(responseData)) {
+            responseData = "IGNORES";
+        }
 
-		log.info(
-				"""
+        log.info(
+                """
+                        ===================Request================>
+                        时间：{}
+                        地址：{}
+                        token：{}
+                        参数：{}
+                        方式：{}
+                        <===================Response================
+                        状态：{}
+                        内容：{}
+                        时长：{}毫秒
+                        ============================================
+                        """,
+                DateUtils.getTime(), requestWrapper.getRequestURL(),
+                // 认证Token
+                ServletUtils.getRequest().get().getHeader(Constants.AUTHORIZATION), reqJson, requestWrapper.getMethod(),
+                responseWrapper.getStatus(), responseData, endTime - startTime
 
-						===================Request================>
-						时间：{}
-						地址：{}
-						token：{}
-						参数：{}
-						方式：{}
-						<===================Response================
-						状态：{}
-						内容：{}
-						时长：{}毫秒
-						============================================""",
-				DateUtils.getTime(), requestWrapper.getRequestURL(),
-				// 认证Token
-				ServletUtils.getRequest().get().getHeader(Constants.AUTHORIZATION), reqJson, requestWrapper.getMethod(),
-				responseWrapper.getStatus(), responseData, endTime - startTime
+        );
 
-		);
+    }
 
-	}
+    private String mapToString(Map<String, String[]> map) {
+        StringBuilder sb = new StringBuilder();
+        for (String key : map.keySet()) {
+            sb.append(key).append("=");
+            String[] paramValues = map.get(key);
+            int len = paramValues.length - 1;
+            for (int i = 0; i <= len; i++) {
 
-	private String mapToString(Map<String, String[]> map) {
-		StringBuilder sb = new StringBuilder();
-		for (String key : map.keySet()) {
-			sb.append(key).append("=");
-			String[] paramValues = map.get(key);
-			int len = paramValues.length - 1;
-			for (int i = 0; i <= len; i++) {
+                sb.append(paramValues[i]);
+                if (i != len) {
+                    sb.append(",");
+                }
+            }
+            sb.append(";");
 
-				sb.append(paramValues[i]);
-				if (i != len) {
-					sb.append(",");
-				}
-			}
-			sb.append(";");
+        }
+        return sb.toString();
+    }
 
-		}
-		return sb.toString();
-	}
+    @Override
+    public void destroy() {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void destroy() {
-		// TODO Auto-generated method stub
-
-	}
+    }
 
 }
