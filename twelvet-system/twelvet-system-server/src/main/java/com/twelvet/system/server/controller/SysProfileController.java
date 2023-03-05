37bb107d36ff7fe72c03ca0535fb139ada269f72
twelvet <2471835953@qq.com>
2023-03-05 19:02:39 +0800

    修复修改密码bug

--- twelvet-system/twelvet-system-server/src/main/java/com/twelvet/system/server/controller/SysProfileController.java
+++ twelvet-system/twelvet-system-server/src/main/java/com/twelvet/system/server/controller/SysProfileController.java
@@ -108,7 +108,7 @@ public class SysProfileController extends TWTController {
 		}
 
 		String username = SecurityUtils.getUsername();
-		SysUser user = userService.selectUserByUserName(username, true);
+		SysUser user = userService.selectUserByUserName(username, false);
 		String password = user.getPassword();
 
 		if (!BCrypt.checkpw(userPassword.getOldPassword(), password)) {
