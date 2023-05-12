0e7ab3cb69bac07abb8546cb3f7045635356857c
TwelveT <2471835953@qq.com>
2023-05-12 15:51:10 +0800

    跨域配置

--- twelvet-admin/src/main/java/com/twelvet/admin/config/CorsConfig.java
+++ twelvet-admin/src/main/java/com/twelvet/admin/config/CorsConfig.java
@@ -14,26 +14,26 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
-    /**
-     * 跨域配置
-     */
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        // 设置访问源地址
-        config.addAllowedOriginPattern("*");
-        // 设置访问源请求头
-        config.addAllowedHeader("*");
-        // 设置访问源请求方法
-        config.addAllowedMethod("*");
-        // 有效期 1800秒
-        config.setMaxAge(1800L);
-        // 添加映射路径，拦截一切请求
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        // 返回新的CorsFilter
-        return new CorsFilter(source);
-    }
+	/**
+	 * 跨域配置
+	 */
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		// 设置访问源地址
+		config.addAllowedOriginPattern("*");
+		// 设置访问源请求头
+		config.addAllowedHeader("*");
+		// 设置访问源请求方法
+		config.addAllowedMethod("*");
+		// 有效期 1800秒
+		config.setMaxAge(1800L);
+		// 添加映射路径，拦截一切请求
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		// 返回新的CorsFilter
+		return new CorsFilter(source);
+	}
 
 }
