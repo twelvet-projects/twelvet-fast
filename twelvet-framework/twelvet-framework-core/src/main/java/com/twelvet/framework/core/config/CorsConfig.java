8b54bc265c34e7e156678eece2185dc70c26aba0
twelvet <2471835953@qq.com>
2023-07-29 11:48:24 +0800

    优化代码生成第二版

--- twelvet-framework/twelvet-framework-core/src/main/java/com/twelvet/framework/core/config/CorsConfig.java
+++ twelvet-framework/twelvet-framework-core/src/main/java/com/twelvet/framework/core/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.twelvet.admin.config;
+package com.twelvet.framework.core.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- twelvet-gen/twelvet-gen-server/src/main/java/com/twelvet/gen/server/controller/GenController.java
+++ twelvet-gen/twelvet-gen-server/src/main/java/com/twelvet/gen/server/controller/GenController.java
@@ -229,8 +229,10 @@ public class GenController extends TWTController {
 	 */
 	private void genCode(HttpServletResponse response, byte[] data) throws IOException {
 		response.reset();
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Expose-Headers", "Content-Disposition");
 		response.setHeader("Content-Disposition", "attachment; filename=\"twelvet.zip\"");
-		response.addHeader("Content-Length", String.valueOf(data.length));
+		response.setHeader("Content-Length", String.valueOf(data.length));
 		response.setContentType("application/octet-stream; charset=UTF-8");
 		IOUtils.write(data, response.getOutputStream());
 	}
