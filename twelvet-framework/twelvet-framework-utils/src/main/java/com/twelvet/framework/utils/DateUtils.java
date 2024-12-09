6ff3ccccc6550db07943b967c3cc0f2904960d75
twelvet <2471835953@qq.com>
2024-12-09 14:28:36 +0800

    :sparkles: 采用LocalDateTime代替Date

--- twelvet-framework/twelvet-framework-utils/src/main/java/com/twelvet/framework/utils/DateUtils.java
+++ twelvet-framework/twelvet-framework-utils/src/main/java/com/twelvet/framework/utils/DateUtils.java
@@ -123,8 +123,7 @@ public class DateUtils extends org.apache.commons.lang3.time.DateUtils {
 	 */
 	public static Date toDate(LocalDate temporalAccessor) {
 		LocalDateTime localDateTime = LocalDateTime.of(temporalAccessor, LocalTime.of(0, 0, 0));
-		ZonedDateTime zdt = localDateTime.atZone(ZoneId.systemDefault());
-		return Date.from(zdt.toInstant());
+        return toDate(localDateTime);
 	}
 
 	/**
