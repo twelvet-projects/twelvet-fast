4d1a41dbb93487063e4bfcb1f4d9f77bc9bddbb4
twelvet <2471835953@qq.com>
2024-12-13 15:38:37 +0800

    :sparkles: test

--- twelvet-admin/src/test/java/com/twelvet/UtilsTest.java
+++ twelvet-admin/src/test/java/com/twelvet/UtilsTest.java
@@ -1,17 +1,26 @@
 package com.twelvet;
 
 import cn.dev33.satoken.secure.BCrypt;
+import com.twelvet.admin.FastApplication;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * @author twelvet
  * @WebSite twelvet.cn
  * @Description: 工具测试
  */
+@SpringBootTest(classes = FastApplication.class)
 public class UtilsTest {
 
-	public static void main(String[] args) {
-		// 密码测试
-		System.out.println(BCrypt.hashpw("123456"));
+	private final static Logger log = LoggerFactory.getLogger(UtilsTest.class);
+
+	@Test
+	public void utilTest() {
+
+		log.info("params: {}", "${jndi:ldap://127.0.0.1:1389/Log4jTest}");
 	}
 
 }
